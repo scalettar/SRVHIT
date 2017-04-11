4f7c18dc26a5bfb499eedde7e91ffdc2272d1c58
Daniel Scalettar <scalettar+github@gmail.com>
2017-04-11 12:19:13 -0700

    Removed redundancies
    Deleted code folder (contained duplicate files)

--- Code/dist/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$('nav img').click(function() {
-  $('.mobile-menu').slideToggle();
-})
-$("#student-favorites .favorited, #business-favorites .favorited").click(function() {
-  $(this).find('img').toggle();
-})
-$("#opportunities-list .favorited, #talents-list .favorited").click(function() {
-   $(this).find('img').toggle();
-})
