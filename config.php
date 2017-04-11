2686fb1750b9546e411e957ac81571e004659027
Daniel Scalettar <scalettar+github@gmail.com>
2017-04-11 15:25:01 -0700

    Minor organizational and text changes
    - Created Login.php and Config.php
    - Minor text fixes (e.g. SRVHT -> SRVHIT)
    - Login form changes

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_SERVER', 'athena.ecs.csus.edu:3306');
+  define('DB_USERNAME', 'teamnull');
+  define('DB_PASSWORD', 'teamnull_db');
+  define('DB_DATABASE', 'teamnull');
+  $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
